6a4a9d51f705f5b6106ded9cb3feb4c3ae4496c7
Isaac Raja <isaacraja@gmail.com>
2010-04-22 16:26:46 +0530

    Problem 1

---
+++ prob1.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* Problem 1
+* http://projecteuler.net/index.php?section=problems&id=1
+*/
+
+function prob1()
+{
+	$sum = 0;
+	for($i = 1; $i < 10; $i++)
+	{
+		if(($i % 3 == 0) || ($i % 5 == 0))
+		{
+			$sum = $sum + $i;
+		}
+	}
+
+	echo $sum;
+}
+prob1();
+?>
